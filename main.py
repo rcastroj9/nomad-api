71f1b9592e64e15680168a11e8d0b5af8f7f5b0c
rcastroj9 <rcastroj@edu.uwaterloo.ca>
2017-11-11 21:54:37 -0500

    Implementing edit_trip and fixing bugs

--- main.py
+++ main.py
@@ -58,13 +58,10 @@ def login(email, password):
     
 def calc_to_save(total_cost, saving_plan, travel_date, saved_amount):
     now = datetime.now()
-    splitted_date = travel_date.split('-');
-    #save the travel_date as an object
-    travel_date = datetime(int(splitted_date[0]), int(splitted_date[1]), int(splitted_date[2]))
     total_time = travel_date - now
     days_in_month = 30
     days_in_year = 365
-    total_cost -= saved_amount
+    total_cost = int(total_cost) - saved_amount
     
     if saving_plan == "monthly":
         to_save = int(total_cost) / total_time.days * days_in_month
@@ -91,6 +88,9 @@ def newTrip(email,trip_name, destination, total_cost, saving_plan, travel_date):
     trip_name = trip_name + email #making trip unique
     #use helper function to calculate how much is necessary to save per saving_plan
     saved_amount = 0
+    splitted_date = travel_date.split('-');
+    #save the travel_date as an object
+    travel_date = datetime(int(splitted_date[0]), int(splitted_date[1]), int(splitted_date[2]))    
     to_save = calc_to_save(total_cost, saving_plan, travel_date, saved_amount)
     
     trips[trip_name] = [destination, total_cost, saving_plan, travel_date, saved_amount, to_save] 
@@ -120,7 +120,7 @@ def delete_trip(email, trip_name):
 @app.route("/edit_trip/<email>/<trip_name>/<changing_param>/<new_value>")
 def edit_trip(email, trip_name, changing_param, new_value):
     #check if the trip the user wants to edit exists
-    if trip_name not in users: 
+    if trip_name not in users[email][2]: 
         return "Trip does not exists, please try with a different one"
     #trip name
     if changing_param == "name": #change it in the users DS as well as in the trips structure
@@ -131,30 +131,31 @@ def edit_trip(email, trip_name, changing_param, new_value):
         trips[new_value + email] = trips.pop(trip_name + email)
         return "Successfully updated trip name"
     #date
-    elif changing_param == "date":
+    trip_name += email
+    if changing_param == "date":
         #change date in trip DS only
         splitted_value = new_value.split('-')
-        new_value = datetime(splitted_value[0], splitted_value[1], splitted_value[2])
+        new_value = datetime(int(splitted_value[0]), int(splitted_value[1]), int(splitted_value[2]))
         trips[trip_name][3] = new_value
-        trips[trip_name][5] = calc_to_save(trips[trip_name][1], trips[trip_name][2], trips[trip_name][3], trips[trip_name[4])
-        return "Successfully updated date now, you will have to save %.2f %s" % to_save, saving_plan
+        trips[trip_name][5] = calc_to_save(trips[trip_name][1], trips[trip_name][2], trips[trip_name][3], trips[trip_name][4])
+        return "Successfully updated date now, you will have to save %.2f %s" % (trips[trip_name][5], trips[trip_name][2])
     #saving_plan
-    elif changing_param == "saving plan":
+    elif changing_param == "saving_plan":
         #change saving plan in trip DS only 
         trips[trip_name][2] = new_value
-        trips[trip_name][5] = calc_to_save(trips[trip_name][1], trips[trip_name][2], trips[trip_name][3], trips[trip_name[4])
-        return "Successfully updated saving plan now, you will have to save %.2f %s" % to_save, saving_plan
+        trips[trip_name][5] = calc_to_save(trips[trip_name][1], trips[trip_name][2], trips[trip_name][3], trips[trip_name][4])
+        return "Successfully updated saving plan now, you will have to save %.2f %s" % (trips[trip_name][5], trips[trip_name][2])
     #cost
     elif changing_param == "cost":
         trips[trip_name][1] = new_value
-        trips[trip_name][5] = calc_to_save(trips[trip_name][1], trips[trip_name][2], trips[trip_name][3], trips[trip_name[4])
-        return "Successfully updated cost of trip now, you will have to save %.2f %s" % to_save, saving_plan
+        trips[trip_name][5] = calc_to_save(trips[trip_name][1], trips[trip_name][2], trips[trip_name][3], trips[trip_name][4])
+        return "Successfully updated cost of trip now, you will have to save %.2f %s" % (trips[trip_name][5], trips[trip_name][2])
     #amount saved
-    elif changing_param == "amount saved":
-        trips[trip_name][4] = new_value
-        trips[trip_name][5] = calc_to_save(trips[trip_name][1], trips[trip_name][2], trips[trip_name][3], trips[trip_name[4])
-        return "Successfully updated you amount saved until now, you wil have to save  %.2f %s" % to_save, saving_plan
+    elif changing_param == "amount_saved":
+        trips[trip_name][4] = int(new_value)
+        trips[trip_name][5] = calc_to_save(trips[trip_name][1], trips[trip_name][2], trips[trip_name][3], trips[trip_name][4])
+        return "Successfully updated you amount saved until now, you wil have to save  %.2f %s" % (trips[trip_name][5], trips[trip_name][2])
 
-@app.
+
 if __name__ == '__main__':
     app.run()
